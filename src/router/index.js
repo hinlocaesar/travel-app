ec44360d89073c207fd52cbb9f2447aad53704af
hinlocaesar <caesar.hinlo.tech@gmail.com>
2025-08-30 14:47:59 +0800

    Add styles and views for travel destinations
    - Created main CSS file with global styles and specific styles for components.
    - Added JSON data for destinations including Brazil, Panama, Hawaii, and Jamaica with their respective experiences.
    - Implemented Vue components for each destination (Brazil, Hawaii, Jamaica, Panama) with basic structure.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import Brazil from '@/views/Brazil.vue'
+import Hawaii from '@/views/Hawaii.vue'
+import Jamaica from '@/views/Jamaica.vue'
+import Panama from '@/views/Panama.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [{path:'/',name:'Home', component:Home},
           {path:'/about',name:'About', component: About},
+          {path:'/brazil',name:'brazil', component: Brazil},
+          {path:'/hawaii',name:'hawaii', component: Hawaii},
+          {path:'/jamaica',name:'jamaica', component: Jamaica},
+          {path:'/panama',name:'Panama', component: Panama}
   ],
 })
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,25 @@
   <template>
-  <h1>Welcome to your home page</h1>
+    <div class="home">
+      <h1>All destinations</h1>
+      <div class="destinations">
+        <router-link v-for="destination in destinations"
+        :key="destination.id"
+        :to="destination.slug">
+          <h2>{{ destination.name }}</h2>
+          <img :src="`/images/${destination.image}`" :alt="destination.name">
+        </router-link>
+        </div>
+    </div>
   </template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+    data() {
+        return {
+            destinations: sourceData.destinations
+        }
+    }
+}
+</script>
