45b584246940eb58571637e80fb028a8aec1779e
hinlocaesar <caesar.hinlo.tech@gmail.com>
2025-08-30 15:41:18 +0800

    Add DestinationShow view and update router for dynamic destination display

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,8 @@ const router = createRouter({
           {path:'/brazil',name:'brazil', component: ()=> import('@/views/Brazil.vue')},
           {path:'/hawaii',name:'hawaii', component: ()=> import('@/views/Hawaii.vue')},
           {path:'/jamaica',name:'jamaica', component: ()=> import('@/views/Jamaica.vue')},
-          {path:'/panama',name:'Panama', component: ()=> import('@/views/Panama.vue')}
+          {path:'/panama',name:'Panama', component: ()=> import('@/views/Panama.vue')},
+          {path: '/destination/:id', component: () => import('@/views/DestinationShow.vue') }
   ],
 })
 
