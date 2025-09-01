8923bbf43ba7ff22480445cd447ee1e674b6672d
hinlocaesar <caesar.hinlo.tech@gmail.com>
2025-09-01 13:24:55 +0800

    Update routing and props handling for DestinationShow view

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,11 @@ import Home from '@/views/Home.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [{path:'/',name:'Home', component:Home},
-          {path: '/destination/:id/:slug', name: 'destination.show', component: () => import('@/views/DestinationShow.vue') }
+          {path: '/destination/:id/:slug',
+          name: 'destination.show',
+          component: () => import('@/views/DestinationShow.vue'),
+          props: route=>({id:parseInt(route.params.id)})
+         }
   ],
 })
 
