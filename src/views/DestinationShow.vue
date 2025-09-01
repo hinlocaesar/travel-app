b16152ef33403487c897d076d2628dff30a1c3aa
hinlocaesar <caesar.hinlo.tech@gmail.com>
2025-09-01 16:40:13 +0800

    Refactor DestinationShow view layout and improve code formatting

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,42 +1,45 @@
 <template>
-    <h1>{{destination.name}}</h1>
-    <Goback/>
+<div>
+  <section v-if="destination" class="destination">
+    <h1>{{ destination.name }}</h1>
+    <GoBack />
     <div class="destination-details">
-      <img :src="`/images/${destination.image}`" :alt="destination.name">
-      <p>{{destination.description}}</p>
+      <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      <p>{{ destination.description }}</p>
     </div>
-        <section class="experiences">
-          <h2>Top experience in {{ destination.name }}</h2>
-          <div class="cards">
-            <router-link
-                  v-for="experience in destination.experiences"
-                  :key="experience.slug"
-                  :to="{ name: 'experience.show', params: {experienceSlug: experience.slug}}"
-            >
-                <ExperienceCard
-                  :experience="experience"
-                  />
-              </router-link>
-          </div>
-          <router-view/>
-      </section>
+  </section>
+  <section class="experiences">
+    <h2>Top Experiences in {{ destination.name }}</h2>
+    <div class="cards">
+      <router-link
+        v-for="experience in destination.experiences"
+        :key="experience.slug"
+        :to="{name: 'experience.show', params:{experienceSlug: experience.slug}}"
+      >
+        <ExperienceCard
+          :experience="experience"
+        />
+      </router-link>
+    </div>
+    <router-view />
+  </section>
+</div>
 </template>
 <script>
-import sourceData from '@/data.json'
-import ExperienceCard from '@/components/ExperienceCard.vue'
-import Goback from '@/components/Goback.vue'
+import sourceData from "@/data.json";
+import ExperienceCard from "@/components/ExperienceCard.vue";
+import GoBack from "@/components/Goback.vue";
 export default {
-  components: {ExperienceCard, Goback},
+  components: { ExperienceCard, GoBack },
   props: {
-    id: {
-      type: Number,
-      required: true
-    }
+    id: { type: Number, required: true },
   },
-  computed:{
-    destination(){
-      return sourceData.destinations.find(destination => destination.id === this.id)
-    }
-  }
-}
+  computed: {
+    destination() {
+      return sourceData.destinations.find(
+        (destination) => destination.id === this.id
+      );
+    },
+  },
+};
 </script>
