af3497505af2135b3d51cd1ed10ed72ed3e3c401
hinlocaesar <caesar.hinlo.tech@gmail.com>
2025-09-02 12:06:56 +0800

    Extending Router Link for External URLs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import AppLink from '@/components/AppLink.vue'
 
 const app = createApp(App)
-
+app.component('AppLink', AppLink)
 app.use(router)
-
 app.mount('#app')
